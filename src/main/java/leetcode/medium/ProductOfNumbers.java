d4815ec8569099ebd2202bd29e42b80e5cf28aca
Jean Hu <yuchun801130@gmail.com>
2020-03-19 22:41:07 +0800

    LeetCode #1352: Product of the Last K Numbers
    Improve run time from O(N) to O(1)

--- src/main/java/leetcode/medium/ProductOfNumbers.java
+++ src/main/java/leetcode/medium/ProductOfNumbers.java
@@ -7,18 +7,22 @@ public class ProductOfNumbers {
   private List<Integer> nums = new ArrayList<>();
 
   public void add(int num) {
-    nums.add(num);
+    if (num == 0)
+      nums = new ArrayList<>();
+    else {
+      int size = nums.size();
+      int product = (size > 0)? nums.get(size - 1) * num : num;
+      nums.add(product);
+    }
   }
 
   public int getProduct(int k) {
-    int product = 1;
-    int lastIdx = nums.size() - 1;
-    for (int i = 0; i < k; i++) {
-      int cur = nums.get(lastIdx - i);
-      if (cur == 0)
-        return 0;
-      product *= cur;
-    }
-    return product;
+    int size = nums.size();
+    if (size < k)
+      return 0;
+    else if (size == k)
+      return nums.get(size - 1);
+    else
+      return nums.get(size - 1) / nums.get(size - k - 1);
   }
 }
